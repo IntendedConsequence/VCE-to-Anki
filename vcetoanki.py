3d667cc05cc85f3ee66fd04fbeca02c362d70175
Irwin1138 <irwin1139@gmail.com>
2011-05-29 17:15:11 +0300

    rewritten the exporter. now it uses regex and looks pretty :3

---
+++ vcetoanki.py
@@ -0,0 +1,40 @@
+import re
+import sys
+import os
+
+lines = ''
+with open("C:\\test.txt", 'r') as f:
+    for line in f:
+        lines += line
+
+exams = [x.strip() for x in re.split(r'^Exam [A-Z]$', lines, flags=re.MULTILINE)[1:]]
+exams = [re.split(r'^QUESTION [0-9][0-9]?[0-9]?', x, flags=re.MULTILINE)[1:] for x in exams]
+exams = [[q.strip() for q in x] for x in exams]
+
+orda = ord('A')
+
+def format_question(raw):
+    question = {}
+    raw = re.split(r'^Explanation\/Reference:', raw, flags=re.S|re.M)
+    question['explanation'] = (raw[1].strip())
+    raw = re.split(r'^Section:', raw[0], flags=re.S|re.M)
+    question['section'] = (raw[1].strip())
+    raw = re.split(r'^Answer:', raw[0], flags=re.S|re.M)
+    question['answer'] = [ord(c) - orda for c in raw[1].strip()]
+    raw = re.split(r'^[A-Z]\.$', raw[0], flags=re.M)
+    question['answers'] = [a.strip() for a in raw[1:]]
+    question['question'] = raw[0].strip()
+    
+    return question
+
+exams = [[format_question(q) for q in e] for e in exams]
+n = '\n'
+br = '<br />'
+
+with open("C:\\output_v2.csv", 'w') as f:
+        for e in exams:
+            for q in e:
+                front = q['question'] + n*3 + 'Answers:\n\n' + (n*2).join([chr(x + orda) + '. ' + q['answers'][x] for x in range(len(q['answers']))])
+                back = (n*2).join([chr(a + orda) + '. ' + q['answers'][a] for a in q['answer']]) + n*3 + q['explanation']
+                
+                f.write(front.replace(n, br) + '~' + back.replace(n, br) + n)
